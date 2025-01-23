e8b6b9912713e888025f9cfb6104973de165f2ef
Oisín Morrison <oisinmmm@gmail.com>
2025-01-23 11:05:52 +0000

    expose from downxy, nextxy, d8 for easier testing

--- src/earthkit/hydro/__init__.py
+++ src/earthkit/hydro/__init__.py
@@ -1,2 +1,7 @@
-from .readers import load_river_network
+from .readers import (
+    from_cama_downxy,
+    from_cama_nextxy,
+    from_d8,
+    load_river_network,
+)
 from .river_network import RiverNetwork

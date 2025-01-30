3682453facb1fc941029362f9357691702ac99b1
Oisín Morrison <oisinmmm@gmail.com>
2025-01-30 11:04:43 +0000

    change import structure

--- src/earthkit/hydro/__init__.py
+++ src/earthkit/hydro/__init__.py
@@ -1,3 +1,5 @@
 from .readers import from_cama_downxy, from_cama_nextxy, from_d8, load_river_network, create_river_network
 from .river_network import RiverNetwork
-from . import accumulation, catchment, subnetwork
+from .accumulation import accuflux, upstream, downstream
+from .subnetwork import create_subnetwork
+from .catchment import catchment, subcatchment

6478ed9949d1baff9f4a134bf82bd4c6a0c6ff86
Oisín Morrison <oisinmmm@gmail.com>
2025-02-12 10:23:59 +0000

    merge _accumulate.py to accumulate.py and _catchment.py to catchment.py

--- src/earthkit/hydro/accumulation.py
+++ src/earthkit/hydro/accumulation.py
@@ -1,10 +1,5 @@
 import numpy as np
-from .utils import mask_and_unmask_data, check_missing
-from ._accumulation import (
-    _ufunc_to_downstream,
-    _ufunc_to_downstream_missing_values_2D,
-    _ufunc_to_downstream_missing_values_ND,
-)
+from .utils import mask_and_unmask_data, check_missing, is_missing
 from .core import flow
 
 
@@ -50,3 +45,88 @@ def flow_downstream(river_network, field, mv=np.nan, in_place=False, ufunc=np.ad
         return op(river_network, field, grouping, mv, ufunc=ufunc)
 
     return flow(river_network, field, False, operation, mv)
+
+
+def _ufunc_to_downstream(river_network, field, grouping, mv, ufunc):
+    """
+    Updates field in-place by applying a ufunc at the downstream nodes of the grouping, ignoring missing values.
+
+    Parameters
+    ----------
+    river_network : earthkit.hydro.RiverNetwork
+        An earthkit-hydro river network object.
+    field : numpy.ndarray
+        The input field.
+    grouping : numpy.ndarray
+        An array of indices.
+    mv : scalar
+        A missing value indicator (not used in the function but kept for consistency).
+    ufunc : numpy.ufunc
+        A universal function from the numpy library to be applied to the field data.
+        Available ufuncs: https://numpy.org/doc/2.2/reference/ufuncs.html. Note: must allow two operands.
+
+    Returns
+    -------
+    None
+    """
+    ufunc.at(field, river_network.downstream_nodes[grouping], field[grouping])
+
+
+def _ufunc_to_downstream_missing_values_2D(river_network, field, grouping, mv, ufunc):
+    """
+    Applies a universal function (ufunc) to downstream nodes in a river network, dealing with missing values for 2D fields.
+
+    Parameters
+    ----------
+    river_network : earthkit.hydro.RiverNetwork
+        An earthkit-hydro river network object.
+    field : numpy.ndarray
+        The input field.
+    grouping : numpy.ndarray
+        An array of indices.
+    mv : scalar
+        A missing value indicator.
+    ufunc : numpy.ufunc
+        A universal function from the numpy library to be applied to the field data.
+        Available ufuncs: https://numpy.org/doc/2.2/reference/ufuncs.html. Note: must allow two operands.
+
+    Returns
+    -------
+    None
+    """
+    nodes_to_update = river_network.downstream_nodes[grouping]
+    values_to_add = field[grouping]
+    missing_indices = np.logical_or(is_missing(values_to_add, mv), is_missing(field[nodes_to_update], mv))
+    ufunc.at(field, nodes_to_update, values_to_add)
+    field[nodes_to_update[missing_indices]] = mv
+
+
+def _ufunc_to_downstream_missing_values_ND(river_network, field, grouping, mv, ufunc):
+    """
+    Applies a universal function (ufunc) to downstream nodes in a river network, dealing with missing values for ND fields.
+
+    Parameters
+    ----------
+    river_network : earthkit.hydro.RiverNetwork
+        An earthkit-hydro river network object.
+    field : numpy.ndarray
+        The input field.
+    grouping : numpy.ndarray
+        An array of indices.
+    mv : scalar
+        A missing value indicator.
+    ufunc : numpy.ufunc
+        A universal function from the numpy library to be applied to the field data.
+        Available ufuncs: https://numpy.org/doc/2.2/reference/ufuncs.html. Note: must allow two operands.
+
+    Returns
+    -------
+    None
+    """
+    nodes_to_update = river_network.downstream_nodes[grouping]
+    values_to_add = field[grouping]
+    missing_indices = np.logical_or(is_missing(values_to_add, mv), is_missing(field[nodes_to_update], mv))
+    ufunc.at(field, nodes_to_update, values_to_add)
+    missing_indices = np.array(np.where(missing_indices))
+    missing_indices[0] = nodes_to_update[missing_indices[0]]
+    field[tuple(missing_indices)] = mv

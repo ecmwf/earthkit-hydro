1d1084b2ce5e99ad7f6fb23d4e102e243a7584e0
oisin-m <oisinmmm@gmail.com>
2025-02-26 20:26:46 +0100

    add support for missing values in calculate_metric_for_labels

--- src/earthkit/hydro/label.py
+++ src/earthkit/hydro/label.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from .metrics import metrics
-from .utils import check_missing, mask_and_unmask_data
+from .utils import check_missing, is_missing
 
 
-@mask_and_unmask_data
-def calculate_metric_for_labels(field, labels, metric, mv=np.nan, accept_missing=False):
-    missing_values_present = check_missing(field, mv, accept_missing)
-    if not missing_values_present or np.isnan(mv):
+def calculate_metric_for_labels(
+    field, labels, metric, field_mv=np.nan, labels_mv=0, field_accept_missing=False
+):
+    missing_values_present = check_missing(field, field_mv, field_accept_missing)
+    if not missing_values_present or np.isnan(field_mv):
         pass
     else:
         # TODO: handle missing values
@@ -15,14 +16,16 @@ def calculate_metric_for_labels(field, labels, metric, mv=np.nan, accept_missing
             "Support for generic missing values not yet implemented."
         )
 
-    input_array = field.ravel()
-    label_array = labels.ravel()
+    mask = ~is_missing(labels, labels_mv)
+
+    input_array = field[mask].ravel()
+    label_array = labels[mask].ravel()
 
     unique_labels, unique_label_positions = np.unique(label_array, return_inverse=True)
 
     initial_field = np.full(len(unique_labels), metric.base_val, dtype=field.dtype)
 
-    metrics.func.at(initial_field, unique_label_positions, input_array)
+    metric.func.at(initial_field, unique_label_positions, input_array)
 
     if metric is metrics.mean:
         count_values = np.bincount(

065bc62a65043310a817633598a61d71f1af9517
oisin-m <oisinmmm@gmail.com>
2025-02-27 11:03:40 +0100

    update to handle ND arrays

--- src/earthkit/hydro/label.py
+++ src/earthkit/hydro/label.py
@@ -7,30 +7,54 @@ from .utils import check_missing, is_missing
 def calculate_metric_for_labels(
     field, labels, metric, field_mv=np.nan, labels_mv=0, field_accept_missing=False
 ):
+
     missing_values_present = check_missing(field, field_mv, field_accept_missing)
-    if not missing_values_present or np.isnan(field_mv):
-        pass
-    else:
+    if missing_values_present and not np.isnan(field_mv):
         # TODO: handle missing values
         raise NotImplementedError(
             "Support for generic missing values not yet implemented."
         )
 
+    labels_ndim = labels.ndim
+    fields_ndim = field.ndim  # fields_ndim = (extra_dims, *labels_ndim)
+
+    labels = labels.T
+
     mask = ~is_missing(labels, labels_mv)
+    not_missing_labels = labels[mask]
 
-    input_array = field[mask].ravel()
-    label_array = labels[mask].ravel()
+    expanded_mask = mask
+    for _ in range(fields_ndim - labels_ndim):
+        expanded_mask = expanded_mask[..., np.newaxis]
 
-    unique_labels, unique_label_positions = np.unique(label_array, return_inverse=True)
+    relevant_field = field.T[mask]
 
-    initial_field = np.full(len(unique_labels), metric.base_val, dtype=field.dtype)
+    unique_labels, unique_label_positions = np.unique(
+        not_missing_labels, return_inverse=True
+    )
 
-    metric.func.at(initial_field, unique_label_positions, input_array)
+    initial_field = np.full(
+        (len(unique_labels), *field.T.shape[labels.ndim :]),
+        metric.base_val,
+        dtype=field.dtype,
+    )
+
+    metric.func.at(
+        initial_field,
+        (unique_label_positions, *[slice(None)] * (initial_field.ndim - 1)),
+        relevant_field,
+    )
 
     if metric is metrics.mean:
         count_values = np.bincount(
             unique_label_positions, minlength=len(unique_labels)
         ).astype(initial_field.dtype)
-        initial_field /= count_values
+        initial_field_T = initial_field.T
+        initial_field_T /= count_values
+        initial_field = initial_field_T.T
+
+    initial_field = np.transpose(
+        initial_field, axes=[0] + list(range(initial_field.ndim - 1, 0, -1))
+    )
 
     return dict(zip(unique_labels, initial_field))

3b2d8fb364d6bde3106863b606547d34446fa0e5
oisin-m <oisinmmm@gmail.com>
2025-03-03 14:20:38 +0100

    improve tests for flow_downstream

--- tests/test_accumulation.py
+++ tests/test_accumulation.py
@@ -7,34 +7,214 @@ import earthkit.hydro as ekh
 
 
 @pytest.mark.parametrize(
-    "river_network, upstream_points",
+    "river_network, input_field, flow_downstream, metric, mv",
     [
-        (("d8_ldd", d8_ldd_1), unit_field_accuflux_1),
-        (("cama_downxy", cama_downxy_1), unit_field_accuflux_1),
-        (("cama_nextxy", cama_nextxy_1), unit_field_accuflux_1),
-        (("d8_ldd", d8_ldd_2), unit_field_accuflux_2),
-        (("cama_downxy", cama_downxy_2), unit_field_accuflux_2),
-        (("cama_nextxy", cama_nextxy_2), unit_field_accuflux_2),
+        (("d8_ldd", d8_ldd_1), input_field_1a, flow_downstream_sum_1a, np.add, mv_1a),
+        (
+            ("d8_ldd", d8_ldd_1),
+            input_field_1a,
+            flow_downstream_max_1a,
+            np.maximum,
+            mv_1a,
+        ),
+        (
+            ("d8_ldd", d8_ldd_1),
+            input_field_1a,
+            flow_downstream_min_1a,
+            np.minimum,
+            mv_1a,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1a,
+            flow_downstream_sum_1a,
+            np.add,
+            mv_1a,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1a,
+            flow_downstream_max_1a,
+            np.maximum,
+            mv_1a,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1a,
+            flow_downstream_min_1a,
+            np.minimum,
+            mv_1a,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1a,
+            flow_downstream_sum_1a,
+            np.add,
+            mv_1a,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1a,
+            flow_downstream_max_1a,
+            np.maximum,
+            mv_1a,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1a,
+            flow_downstream_min_1a,
+            np.minimum,
+            mv_1a,
+        ),
+        (("d8_ldd", d8_ldd_1), input_field_1b, flow_downstream_sum_1b, np.add, mv_1b),
+        (
+            ("d8_ldd", d8_ldd_1),
+            input_field_1b,
+            flow_downstream_max_1b,
+            np.maximum,
+            mv_1b,
+        ),
+        (
+            ("d8_ldd", d8_ldd_1),
+            input_field_1b,
+            flow_downstream_min_1b,
+            np.minimum,
+            mv_1b,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1b,
+            flow_downstream_sum_1b,
+            np.add,
+            mv_1b,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1b,
+            flow_downstream_max_1b,
+            np.maximum,
+            mv_1b,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1b,
+            flow_downstream_min_1b,
+            np.minimum,
+            mv_1b,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1b,
+            flow_downstream_sum_1b,
+            np.add,
+            mv_1b,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1b,
+            flow_downstream_max_1b,
+            np.maximum,
+            mv_1b,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1b,
+            flow_downstream_min_1b,
+            np.minimum,
+            mv_1b,
+        ),
+        (("d8_ldd", d8_ldd_1), input_field_1g, flow_downstream_sum_1g, np.add, mv_1g),
+        (
+            ("d8_ldd", d8_ldd_1),
+            input_field_1g,
+            flow_downstream_max_1g,
+            np.maximum,
+            mv_1g,
+        ),
+        (
+            ("d8_ldd", d8_ldd_1),
+            input_field_1g,
+            flow_downstream_min_1g,
+            np.minimum,
+            mv_1g,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1g,
+            flow_downstream_sum_1g,
+            np.add,
+            mv_1g,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1g,
+            flow_downstream_max_1g,
+            np.maximum,
+            mv_1g,
+        ),
+        (
+            ("cama_downxy", cama_downxy_1),
+            input_field_1g,
+            flow_downstream_min_1g,
+            np.minimum,
+            mv_1g,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1g,
+            flow_downstream_sum_1g,
+            np.add,
+            mv_1g,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1g,
+            flow_downstream_max_1g,
+            np.maximum,
+            mv_1g,
+        ),
+        (
+            ("cama_nextxy", cama_nextxy_1),
+            input_field_1g,
+            flow_downstream_min_1g,
+            np.minimum,
+            mv_1g,
+        ),
     ],
     indirect=["river_network"],
 )
 @pytest.mark.parametrize("N", range(4))
-def test_accumulate_downstream(river_network, upstream_points, N):
-    extra_dims = [np.random.randint(10) for _ in range(N)]
-    field = np.ones((*extra_dims, river_network.n_nodes), dtype=int)
-    accum = ekh.flow_downstream(river_network, field)
-    print(accum[..., :])
-    print(upstream_points)
-    extended_upstream_points = np.tile(upstream_points, extra_dims + [1])
-    np.testing.assert_array_equal(accum, extended_upstream_points)
+def test_flow_downstream(river_network, input_field, flow_downstream, metric, mv, N):
+    print("input", input_field)
+    output_field = ekh.flow_downstream(
+        river_network,
+        input_field,
+        mv,
+        in_place=False,
+        ufunc=metric,
+        accept_missing=True,
+    )
+    print("output", output_field)
+    print(flow_downstream)
+    np.testing.assert_array_equal(output_field, flow_downstream)
+
+
+# def test_flow_downstream(river_network, flow_downstream, metric, mv, N):
+#     extra_dims = [np.random.randint(10) for _ in range(N)]
+#     field = np.ones((*extra_dims, river_network.n_nodes), dtype=int)
+#     output_field = ekh.flow_downstream(river_network, field)
+#     print(accum[..., :])
+#     print(upstream_points)
+#     extended_upstream_points = np.tile(upstream_points, extra_dims + [1])
+#     np.testing.assert_array_equal(accum, extended_upstream_points)
 
 
 @pytest.mark.parametrize(
     "river_network, input_field, accum_field",
     [
-        (("d8_ldd", d8_ldd_1), input_field_accuflux_1, field_accuflux_1),
-        (("cama_downxy", cama_downxy_1), input_field_accuflux_1, field_accuflux_1),
-        (("cama_nextxy", cama_nextxy_1), input_field_accuflux_1, field_accuflux_1),
+        (("d8_ldd", d8_ldd_1), input_field_1b, flow_downstream_sum_1g),
+        (("cama_downxy", cama_downxy_1), input_field_1b, flow_downstream_sum_1g),
+        (("cama_nextxy", cama_nextxy_1), input_field_1b, flow_downstream_sum_1g),
         (("d8_ldd", d8_ldd_2), input_field_accuflux_2, field_accuflux_2),
         (("cama_downxy", cama_downxy_2), input_field_accuflux_2, field_accuflux_2),
         (("cama_nextxy", cama_nextxy_2), input_field_accuflux_2, field_accuflux_2),
--- tests/test_inputs/accumulation.py
+++ tests/test_inputs/accumulation.py
@@ -1,25 +1,82 @@
 import numpy as np
 
-unit_field_accuflux_1 = np.array(
+# RIVER NETWORK ONE
+
+# 1a: unit field input
+input_field_1a = np.array(
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype=int
+)
+
+mv_1a = np.nan
+
+flow_downstream_sum_1a = np.array(
     [1, 1, 1, 1, 1, 2, 2, 3, 2, 1, 3, 3, 9, 3, 1, 1, 20, 3, 2, 1]
 )
 
+flow_downstream_max_1a = np.array(
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+)
+
+flow_downstream_min_1a = np.array(
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+)
+
+# 1b: non-missing integer field input
+input_field_1b = np.array(
+    [1, 2, 3, -1, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1], dtype=int
+)
+
+mv_1b = np.nan
+
+flow_downstream_sum_1b = np.array(
+    [1, 2, 3, -1, 5, 7, 9, 10, 14, 10, 8, 11, 46, 19, 5, 6, 94, 16, 8, -1]
+)
+
+flow_downstream_max_1b = np.array(
+    [1, 2, 3, -1, 5, 6, 7, 8, 9, 10, 6, 7, 10, 10, 5, 6, 10, 9, 9, -1]
+)
+
+flow_downstream_min_1b = np.array(
+    [1, 2, 3, -1, 5, 1, 2, -1, 5, 10, 1, 2, -1, 4, 5, 6, -1, -1, -1, -1]
+)
+
+# 1c: non-missing float field input
+
+# 1d: non-missing bool field input
+
+# 1e: missing float field input with mv=np.nan
+
+# 1f: missing float field input with mv=0
+
+# 1g: missing integer field input with mv=-1
+
+input_field_1g = np.array(
+    [1, 2, 3, -1, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1], dtype=int
+)
+
+mv_1g = -1
+
+flow_downstream_sum_1g = np.array(
+    [1, 2, 3, -1, 5, 7, 9, -1, 14, 10, 8, 11, -1, 19, 5, 6, -1, -1, -1, -1]
+)
+
+flow_downstream_max_1g = np.array(
+    [1, 2, 3, -1, 5, 6, 7, -1, 9, 10, 6, 7, -1, 10, 5, 6, -1, -1, -1, -1]
+)
+
+flow_downstream_min_1g = np.array(
+    [1, 2, 3, -1, 5, 1, 2, -1, 5, 10, 1, 2, -1, 4, 5, 6, -1, -1, -1, -1]
+)
+
+# 1h: missing bool field input with mv=False
+
+
+# RIVER NETWORK TWO
 
 unit_field_accuflux_2 = np.array([2, 1, 2, 1, 1, 2, 7, 3, 1, 1, 10, 6, 1, 13, 1, 2])
 
-
-input_field_accuflux_1 = np.array(
-    [1, 2, 3, -1, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1]
-)
-
-
 input_field_accuflux_2 = np.array(
     [1, 2, -1, 4, 5, 6, 7, 8, 9, 10, 11, 12, -1, 14, 15, 16]
-)
-
-
-field_accuflux_1 = np.array(
-    [1, 2, 3, -1, 5, 7, 9, -1, 14, 10, 8, 11, -1, 19, 5, 6, -1, -1, -1, -1]
 )
 
 

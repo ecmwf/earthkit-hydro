bugfix std and var online computation

src/earthkit/hydro/upstream.py
@@ -46,13 +46,13 @@ def calculate_upstream_metric(
     field, field_dtype = missing_to_nan(field.copy(), mv, accept_missing)
 
     if weights is None:
-        if metric == "mean" or metric == "stdev" or metric == "var":
-            weights = np.ones(river_network.n_nodes, dtype=np.float64)
+        if metric == "mean" or metric == "std" or metric == "var":
+            weightings = np.ones(river_network.n_nodes, dtype=np.float64)
         weighted_field = field.copy()
     else:
         assert field_dtype == weights.dtype
-        weights, _ = missing_to_nan(weights.copy(), mv, accept_missing)
-        weighted_field = field * weights  # this isn't in_place !
+        weightings, _ = missing_to_nan(weights.copy(), mv, accept_missing)
+        weighted_field = field * weightings  # this isn't in_place !
 
     ufunc = metrics_dict[metric].func
 
@@ -67,26 +67,27 @@ def calculate_upstream_metric(
         skip=True,
     )
 
-    if metric == "mean" or metric == "stdev" or metric == "var":
+    if metric == "mean" or metric == "std" or metric == "var":
         counts = flow_downstream(
             river_network,
-            weights,
+            weightings,
             np.nan,  # mv replaced by nan
-            True,  # do in-place on field copy
+            False,
             ufunc,
             accept_missing,
             skip_missing_check=True,
             skip=True,
         )
-        weighted_field /= counts
+
         if metric == "mean":
+            weighted_field /= counts  # weighted mean
             return nan_to_missing(
                 weighted_field, np.float64, mv
             )  # if we compute means, we change dtype for int fields etc.
-        else:
+        elif metric == "var" or metric == "std":
             weighted_sum_of_squares = flow_downstream(
                 river_network,
-                weights * (field - weighted_field) ** 2,
+                field**2 * weightings if weights is not None else field**2,
                 np.nan,  # mv replaced by nan
                 True,  # do in-place on field copy
                 ufunc,
@@ -94,10 +95,17 @@ def calculate_upstream_metric(
                 skip_missing_check=True,
                 skip=True,
             )
+            mean = weighted_field / counts
+            weighted_sum_of_squares = (
+                mean**2 * counts - 2 * mean * weighted_field + weighted_sum_of_squares
+            )
+            weighted_sum_of_squares[weighted_sum_of_squares < 0] = (
+                0  # can occur for numerical issues
+            )
             weighted_sum_of_squares /= counts
             if metric == "var":
                 return nan_to_missing(weighted_sum_of_squares, np.float64, mv)
-            elif metric == "stdev":
+            elif metric == "std":
                 return nan_to_missing(np.sqrt(weighted_sum_of_squares), np.float64, mv)
 
     else:

src/earthkit/hydro/zonal.py
@@ -81,7 +81,7 @@ def calculate_zonal_metric(
         weighted_field,
     )
 
-    if metric == "mean" or metric == "var" or metric == "stdev":
+    if metric == "mean" or metric == "var" or metric == "std":
         if weights is None:
             count_values = np.bincount(
                 unique_label_positions, minlength=len(unique_labels)
@@ -103,7 +103,7 @@ def calculate_zonal_metric(
 
         field_dtype = np.float64
 
-        if metric == "var" or metric == "stdev":
+        if metric == "var" or metric == "std":
             out_field = np.full(
                 (len(unique_labels), *field.T.shape[labels.ndim :]),
                 metrics_dict[metric].base_val,
@@ -120,7 +120,7 @@ def calculate_zonal_metric(
                 ),
             )
             initial_field = (out_field.T / count_values.T).T
-            if metric == "stdev":
+            if metric == "std":
                 initial_field = np.sqrt(initial_field)
 
     initial_field = nan_to_missing(initial_field, field_dtype, field_mv)

tests/test_accumulation.py
@@ -497,7 +497,7 @@ def test_accumulate_downstream_2d(river_network, N):
     ],
     indirect=True,
 )
-@pytest.mark.parametrize("metric", ["sum", "max", "min", "mean"])
+@pytest.mark.parametrize("metric", ["sum", "max", "min", "mean", "std", "var"])
 def test_calculate_catchment(river_network, metric):
     field = np.random.rand(3, 4, river_network.n_nodes)
     weights = np.random.rand(3, 4, river_network.n_nodes)
@@ -525,10 +525,11 @@ def test_calculate_catchment(river_network, metric):
     ],
     indirect=True,
 )
-@pytest.mark.parametrize("metric", ["sum", "max", "min", "mean"])
+@pytest.mark.parametrize("metric", ["sum", "max", "min", "mean", "std", "var"])
 def test_calculate_subcatchment(river_network, metric):
-    field = np.random.rand(3, 4, river_network.n_nodes)
-    weights = np.random.rand(3, 4, river_network.n_nodes)
+    shape = (3, 4)
+    field = np.random.rand(*shape, river_network.n_nodes)
+    weights = np.random.rand(*shape, river_network.n_nodes)
     subcatchment_metric = ekh.calculate_subcatchment_metric(
         river_network, field, river_network.sinks, metric, weights
     )
@@ -537,8 +538,11 @@ def test_calculate_subcatchment(river_network, metric):
     )
     for i in catchment_metric.keys():
         assert catchment_metric[i].dtype == subcatchment_metric[i].dtype
-        np.testing.assert_allclose(subcatchment_metric[i], catchment_metric[i])
-
+        # the two methods don't match always because of numerical instabilities in the
+        # variance computation arising from the different scales
+        np.testing.assert_allclose(
+            subcatchment_metric[i], catchment_metric[i], atol=5e-8
+        )
     if metric == "sum":
         subcatchment_metric = ekh.calculate_subcatchment_metric(
             river_network, field, river_network.nodes, metric, weights
c48b6b21bd0d61c9bc96c3437211114224ff7a82
oisin-m <oisinmmm@gmail.com>
2025-04-14 12:06:18 +0200

    add downstream module

---
+++ src/earthkit/hydro/downstream.py
@@ -0,0 +1,51 @@
+from functools import partial
+
+import numpy as np
+
+from .accumulation import calculate_online_metric
+from .metrics import metrics_dict
+
+
+def calculate_downstream_metric(
+    river_network,
+    field,
+    metric,
+    weights=None,
+    mv=np.nan,
+    accept_missing=False,
+):
+    """
+    Calculates a metric for the field over all downstream values.
+
+    Parameters
+    ----------
+    river_network : earthkit.hydro.RiverNetwork
+        An earthkit-hydro river network object.
+    field : numpy.ndarray
+        The input field.
+    metric : str
+        Metric to compute. Options are "mean", "max", "min", "sum", "product"
+    weights : ndarray, optional
+        Used to weight the field when computing the metric. Default is None.
+    mv : scalar, optional
+        Missing value for the input field. Default is np.nan.
+    accept_missing : bool, optional
+        Whether or not to accept missing values in the input field. Default is False.
+
+    Returns
+    -------
+    numpy.ndarray
+        Output field.
+
+    """
+
+    return calculate_online_metric(
+        river_network, field, metric, weights, mv, accept_missing, flow_direction="up"
+    )
+
+
+for metric in metrics_dict.keys():
+
+    func = partial(calculate_downstream_metric, metric=metric)
+
+    globals()[metric] = func

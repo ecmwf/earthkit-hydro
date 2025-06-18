c8b2108e1ccff29c8d81daf15e63393e256f95c0
oisin-m <oisinmmm@gmail.com>
2025-06-18 20:07:03 +0100

    support bifurcations in move_upstream and move_downstream

--- src/earthkit/hydro/movement.py
+++ src/earthkit/hydro/movement.py
@@ -15,6 +15,8 @@ from .utils import mask_and_unmask, missing_to_nan, nan_to_missing
 def move_downstream(
     river_network,
     field,
+    node_weights,
+    edge_weights,
     mv=np.nan,
     ufunc=np.add,
     accept_missing=False,
@@ -40,24 +42,62 @@ def move_downstream(
         The updated field with upstream contributions.
 
     """
-    assert not river_network.has_bifurcations
     field, field_dtype = missing_to_nan(field, mv, accept_missing)
+    if node_weights is not None:
+        if field_dtype != node_weights.dtype:
+            raise ValueError(
+                f"""
+                node_weights.dtype={node_weights.dtype} but field.dtype={field_dtype}.
+                """
+            )
+        node_weights, _ = missing_to_nan(node_weights.copy(), mv, accept_missing)
+    if edge_weights is not None:
+        if field_dtype != edge_weights.dtype:
+            raise ValueError(
+                f"""
+                edge_weights.dtype={edge_weights.dtype} but field.dtype={field_dtype}.
+                """
+            )
+        edge_weights, _ = missing_to_nan(edge_weights.copy(), mv, accept_missing)
 
     ups = np.zeros(field.shape, dtype=field_dtype)
-    mask = (
-        river_network.downstream_nodes != river_network.n_nodes
-    )  # remove sinks since they have no downstream
-    nodes_to_update = river_network.downstream_nodes[mask]
-    values_to_add = field[..., mask]
+    if not river_network.has_bifurcations:
+        mask = (
+            river_network.downstream_nodes != river_network.n_nodes
+        )  # remove sinks since they have no downstream
+        nodes_to_update = river_network.downstream_nodes[mask]
+        values_to_add = (
+            field[..., mask]
+            if node_weights is None
+            else field[..., mask] * node_weights[..., mask]
+        )
+    else:
+        nodes_to_update = river_network.down_ids
+        values_to_add = (
+            field[..., river_network.up_ids]
+            if node_weights is None
+            else field[..., river_network.up_ids]
+            * node_weights[..., river_network.up_ids]
+        )
+
+    if edge_weights is not None:
+        values_to_add *= edge_weights
+
     ufunc.at(ups, (*[slice(None)] * (ups.ndim - 1), nodes_to_update), values_to_add)
-
     ups = nan_to_missing(ups, field_dtype, mv)
-
     return ups
 
 
 @mask_and_unmask
-def move_upstream(river_network, field, mv=np.nan, accept_missing=False):
+def move_upstream(
+    river_network,
+    field,
+    node_weights,
+    edge_weights,
+    mv=np.nan,
+    ufunc=np.add,
+    accept_missing=False,
+):
     """Sets each node to be its downstream node value, or a missing value.
 
     Parameters
@@ -77,12 +117,45 @@ def move_upstream(river_network, field, mv=np.nan, accept_missing=False):
         The updated field with downstream values.
 
     """
-    assert not river_network.has_bifurcations
     field, field_dtype = missing_to_nan(field, mv, accept_missing)
 
+    if node_weights is not None:
+        if field_dtype != node_weights.dtype:
+            raise ValueError(
+                f"""
+                node_weights.dtype={node_weights.dtype} but field.dtype={field_dtype}.
+                """
+            )
+        node_weights, _ = missing_to_nan(node_weights.copy(), mv, accept_missing)
+    if edge_weights is not None:
+        if field_dtype != edge_weights.dtype:
+            raise ValueError(
+                f"""
+                edge_weights.dtype={edge_weights.dtype} but field.dtype={field_dtype}.
+                """
+            )
+        edge_weights, _ = missing_to_nan(edge_weights.copy(), mv, accept_missing)
+
     down = np.zeros(field.shape, dtype=field_dtype)
-    mask = river_network.downstream_nodes != river_network.n_nodes  # remove sinks
-    down[..., mask] = field[..., river_network.downstream_nodes[mask]]
+    if not river_network.has_bifurcations:
+        mask = river_network.downstream_nodes != river_network.n_nodes  # remove sinks
+        update_vals = field[..., river_network.downstream_nodes[mask]]
+        if node_weights is not None:
+            update_vals *= node_weights[..., river_network.downstream_nodes[mask]]
+        if edge_weights is not None:
+            update_vals *= edge_weights
+        down[..., mask] = update_vals
+    else:
+        nodes_to_update = river_network.up_ids
+        update_vals = (
+            field[..., river_network.down_ids]
+            if node_weights is None
+            else field[..., river_network.down_ids]
+            * node_weights[..., river_network.down_ids]
+        )
+        if edge_weights is not None:
+            update_vals *= edge_weights
+        ufunc.at(down, (*[slice(None)] * (down.ndim - 1), nodes_to_update), update_vals)
 
     down = nan_to_missing(down, field_dtype, mv)
 

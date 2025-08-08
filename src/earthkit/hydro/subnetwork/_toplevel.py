351be1be5790c103fdd13d6994b10148943beeed
oisin-m <oisinmmm@gmail.com>
2025-08-08 16:44:56 +0200

    wip: subnetwork with optional edge or node mask

--- src/earthkit/hydro/subnetwork/_toplevel.py
+++ src/earthkit/hydro/subnetwork/_toplevel.py
@@ -13,11 +13,19 @@ def create(river_network: RiverNetwork, node_mask=None, edge_mask=None, copy=Tru
     """
     if river_network.array_backend != "numpy" or copy is not True:
         raise NotImplementedError
+    
+    if (node_mask is None and edge_mask is None):
+        return cp.deepcopy(river_network)
 
     storage = cp.deepcopy(river_network._storage)
-    valid_edges = edge_mask & (
-        node_mask[storage.sorted_data[0]] & node_mask[storage.sorted_data[1]]
-    )
+    if edge_mask is not None and node_mask is not None:
+        valid_edges = edge_mask & (
+            node_mask[storage.sorted_data[0]] & node_mask[storage.sorted_data[1]]
+        )
+    elif edge_mask is None:
+        valid_edges = node_mask[storage.sorted_data[0]] & node_mask[storage.sorted_data[1]]
+    else:
+        valid_edges = edge_mask
     storage.sorted_data = storage.sorted_data[..., valid_edges]
     storage.splits = np.cumsum(valid_edges)[storage.splits - 1]
     storage.mask = storage.mask[node_mask]

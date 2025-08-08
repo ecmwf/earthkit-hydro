59381aca967473d29a77e15d3a227aff53324951
oisin-m <oisinmmm@gmail.com>
2025-08-08 16:45:27 +0200

    wip: subnetwork from grid mask

--- src/earthkit/hydro/subnetwork/_toplevel.py
+++ src/earthkit/hydro/subnetwork/_toplevel.py
@@ -1,21 +1,43 @@
 import copy as cp
 
-import numpy as np
-
+from earthkit.hydro._backends.numpy_backend import NumPyBackend
+from earthkit.hydro._utils.decorators.masking import mask_last2_dims
 from earthkit.hydro.data_structures import RiverNetwork
+
+np = NumPyBackend()
 
 
 def create(river_network: RiverNetwork, node_mask=None, edge_mask=None, copy=True):
     """
     Create a subnetwork from a RiverNetwork.
 
-    TODO: finish
+    Parameters
+    ----------
+    river_network : RiverNetwork
+        Original river network from which to create a subnetwork.
+    node_mask : array, optional
+        A mask of the network nodes.
+    edge_mask : array, optional
+        A mask of the network edges.
+    copy : bool, optional
+        Whether or not to modify the original river network or return a copy.
+
+    Returns
+    -------
+    RiverNetwork
+        The river network object created from the given data.
     """
     if river_network.array_backend != "numpy" or copy is not True:
         raise NotImplementedError
-    
-    if (node_mask is None and edge_mask is None):
+
+    if node_mask is None and edge_mask is None:
         return cp.deepcopy(river_network)
+
+    if node_mask is not None:
+        if node_mask.shape[-2:] == river_network.shape:
+            node_mask = mask_last2_dims(
+                np, node_mask, river_network.mask, node_mask.shape
+            )
 
     storage = cp.deepcopy(river_network._storage)
     if edge_mask is not None and node_mask is not None:
@@ -23,7 +45,9 @@ def create(river_network: RiverNetwork, node_mask=None, edge_mask=None, copy=Tru
             node_mask[storage.sorted_data[0]] & node_mask[storage.sorted_data[1]]
         )
     elif edge_mask is None:
-        valid_edges = node_mask[storage.sorted_data[0]] & node_mask[storage.sorted_data[1]]
+        valid_edges = (
+            node_mask[storage.sorted_data[0]] & node_mask[storage.sorted_data[1]]
+        )
     else:
         valid_edges = edge_mask
     storage.sorted_data = storage.sorted_data[..., valid_edges]

bugfix edge weight normalisation

src/earthkit/hydro/_readers/readers.py
@@ -333,7 +333,7 @@ def from_grit(path):
 
     lines = gpd.read_file(path, layer="lines")
     ref = gdf["global_id"]
-    value_to_index = pd.Series(ref.index, index=ref).to_dict()
+    value_to_index = dict(zip(ref.values, ref.index.values))
     lines["UPID"] = lines["upstream_node_id"].map(value_to_index)
     lines["DOWNID"] = lines["downstream_node_id"].map(value_to_index)
     lines.sort_values(by=["UPID"], inplace=True)
@@ -341,6 +341,7 @@ def from_grit(path):
     down_ids = lines["DOWNID"].to_numpy()
     nodes = ref.index.values
     edge_weights = lines["width_adjusted"].to_numpy()
+    np.nan_to_num(edge_weights, copy=False, nan=1)
 
     shape = rows.max() + 1, cols.max() + 1
 

src/earthkit/hydro/data_structures/_network.py
@@ -53,16 +53,21 @@ class RiverNetwork:
         self.data = [self._storage.sorted_data]
         self.groups = np.split(self._storage.sorted_data, self._storage.splits, axis=1)
 
-        # if self.bifurcates:
-        #     from earthkit.hydro._core._move import move_python
-        #     from earthkit.hydro._core.metrics import metrics_func_finder
-        #     from earthkit.hydro._backends.numpy_backend import NumPyBackend
-        #     numpy_backend = NumPyBackend()
-        #     func = metrics_func_finder("sum", numpy_backend).func
-        #     edge_value_at_nodes = move_python(numpy_backend, self, np.zeros(self.n_nodes), func, invert_graph=True, edge_additive_weight=self.edge_weights)
-        #     print(edge_value_at_nodes)
-        #     print(edge_value_at_nodes[self._storage.sorted_data[1]])
-        #     self.edge_weights /= edge_value_at_nodes[self._storage.sorted_data[1]]
+        if self.bifurcates:
+            from earthkit.hydro.move.array import upstream
+
+            edge_weights_per_node = upstream(
+                self,
+                np.ones(self.n_nodes),
+                edge_weights=self.edge_weights,
+                return_type="masked",
+            )
+            edge_weights_norm = np.empty(self.n_edges)
+            edge_weights_norm[self._storage.sorted_data[2]] = edge_weights_per_node[
+                self._storage.sorted_data[1]
+            ]
+            del edge_weights_per_node
+            self.edge_weights /= edge_weights_norm
 
     def __str__(self):
         return f"RiverNetwork with {self.n_nodes} nodes and {self.n_edges} edges."
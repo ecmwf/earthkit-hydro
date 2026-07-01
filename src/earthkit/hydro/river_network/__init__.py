e72200596ff24237de7c3e5c7fcbf562da7e4d76
oisin-m <oisinmmm@gmail.com>
2026-07-01 16:22:47 +0200

    feat: add clean export method

--- src/earthkit/hydro/river_network/__init__.py
+++ src/earthkit/hydro/river_network/__init__.py
@@ -1,3 +1,5 @@
+from ._export import export
+from ._repair import repair
 from ._river_network import available, create, load
 
-__all__ = ["available", "create", "load"]
+__all__ = ["available", "create", "load", "repair", "export"]
--- src/earthkit/hydro/river_network/_export.py
+++ src/earthkit/hydro/river_network/_export.py
@@ -1,12 +1,37 @@
+from typing import Union
+
 import numpy as np
 import xarray as xr
 
+from earthkit.hydro.data_structures import RiverNetwork
 from earthkit.hydro.data_structures._network_storage import RiverNetworkStorage
 
 
 def export(
-    river_network_storage: RiverNetworkStorage, river_network_format: str, fpath: str
+    river_network: Union[RiverNetworkStorage, RiverNetwork],
+    path: str,
+    river_network_format: str = "precomputed",
+    compression=1,
 ):
+    if river_network_format not in ["precomputed", "pcr_d8", "esri_d8", "merit_d8"]:
+        raise ValueError(
+            f"Exporting river network to format {river_network_format} is not currently supported."
+        )
+
+    river_network_storage = (
+        river_network
+        if isinstance(river_network, RiverNetworkStorage)
+        else river_network._storage
+    )
+
+    if river_network_format == "precomputed":
+        import joblib
+
+        joblib.dump(river_network_storage, path, compress=compression)
+        return
+
+    missing_values = {"pcr_d8": 255, "esri_d8": 255, "merit_d8": 247}
+
     d, u, _ = river_network_storage.sorted_data
     mask = river_network_storage.mask
     coords = river_network_storage.coords
@@ -20,8 +45,9 @@ def export(
     dy[u] = (mask[d] // nx) - (mask[u] // nx)
 
     # Scatter back to 2D grids
-    downx = np.full(shape, dtype=int, fill_value=-1)
-    downy = np.full(shape, dtype=int, fill_value=-1)
+    mv = missing_values[river_network_format]
+    downx = np.full(shape, dtype=int, fill_value=mv)
+    downy = np.full(shape, dtype=int, fill_value=mv)
     downx.flat[mask] = dx
     downy.flat[mask] = dy
 
@@ -34,7 +60,10 @@ def export(
             ],
             dtype=np.uint8,
         )
-        data = lut[downy + 1, downx + 1]
+        try:
+            data = lut[downy + 1, downx + 1]
+        except Exception as e:
+            raise ValueError("Failed to represent river network as D8") from e
     elif river_network_format == "esri_d8" or river_network_format == "merit_d8":
         lut = np.array(
             [
@@ -44,13 +73,21 @@ def export(
             ],
             dtype=np.uint8,
         )
-        data = lut[downy + 1, downx + 1]
+        try:
+            data = lut[downy + 1, downx + 1]
+        except Exception as e:
+            raise ValueError("Failed to represent river network as D8") from e
     else:
         raise ValueError(
             f"Unsupported river network format for the export method: {river_network_format}."
         )
 
     coord1, coord2 = coords.keys()
-    da = xr.DataArray(data, dims=(coord1, coord2), coords=coords, name="ldd")
+    da = xr.DataArray(
+        data.astype(np.uint8), dims=(coord1, coord2), coords=coords, name="ldd"
+    )
     da.attrs["generated_by"] = "earthkit-hydro"
-    da.to_netcdf(fpath)
+    da.encoding = {
+        "_FillValue": mv,  # Map np.nan to -9999 on disk
+    }
+    da.to_netcdf(path)
